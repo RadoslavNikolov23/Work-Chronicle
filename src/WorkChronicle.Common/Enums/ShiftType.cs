3ce86dee80e4c626c25343059738820d7a8a42e0
Radoslav Nikolov <156640386+RadoslavNikolov23@users.noreply.github.com>
2025-06-02 11:07:53 +0300

    Delete src/WorkChronicle.Common directory

--- src/WorkChronicle.Common/Enums/ShiftType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WorkChronicle.Common.Enums
-{
-    public enum ShiftType
-    {
-        DayShift = 0,
-        NightShift = 1,
-        RestDay = 2
-    }
-}
--- src/WorkChronicle.Common/HoursCoefficients/HourCoefficient.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WorkChronicle.Common.HoursCoefficients
-{
-    public static class HourCoefficient
-    {
-        public const double NightShiftCoefficient = 0.143;
-    }
-}
